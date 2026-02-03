05fd389686da66f92aa2784c5fea57e933e63f91
Digvijay Rawat <digvijay.rawat@gmail.com>
2026-02-03 17:49:53 +0530

    refactored and added comments

--- GelatoMaker.py
+++ GelatoMaker.py
@@ -9,10 +9,81 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+def get_gelato_cost_and_weight(total_cost: float, 
+                               total_water: float, 
+                               total_weight: float, 
+                               selected_ingredients: dict, 
+                               ingredient_amount: dict) -> tuple[float, float, float]:
+    '''
+    Function to calculate the total weight, cost, and water content of the base mix.
+    '''
+    for ingredient, ingredient_info in selected_ingredients.items():
+        total_water += (ingredient_info['Water (%)']/100) * ingredient_amount[ingredient]
+        if ingredient == 'Milk': # as milk amount is in ml not grams
+            total_weight += (ingredient_amount[ingredient] * milk_density)
+        else:
+            total_weight += ingredient_amount[ingredient]
+        total_cost += ingredient_info['Cost/g'] * ingredient_amount[ingredient]
+    return total_cost, total_water, total_weight
 
-def get_total_nutrition(ingredient, label, nutrition_df, ingredient_amount, selected_ingredients):
+
+def get_nutrition(ingredient: str, 
+                  label: str, 
+                  nutrition_df: pd.DataFrame, 
+                  ingredient_amount: dict, 
+                  selected_ingredients: dict) -> float:
+    '''
+    Function to calculate the total amount of a nutritional element (fat, sugar, etc) coming from
+    a single ingredient.
+    '''
     return nutrition_df.loc[selected_ingredients[ingredient]['Name'], label]/100 * ingredient_amount
 
+
+def get_gelato_nutritional_information(nutritional_information: dict):
+    '''
+    Function to calculate nutritional information per 100grams of the end product gelato
+    
+    All ingredients are considered - ingredients that go into the base and ingredients that 
+    are add ons, such as melted chocolate in stracciatela.
+    '''
+    for key in nutritional_information.keys():
+        for ingredient in selected_ingredients.keys():
+            nutritional_information[key] += get_nutrition(ingredient, key, nutrition_df, 
+                                                                ingredient_amount[ingredient], selected_ingredients)
+        nutritional_information[key] = nutritional_information[key] / total_weight * 100 
+    nutritional_information_df = pd.DataFrame.from_dict(nutritional_information, orient = 'index', 
+                                                        columns = ['Value per 100 grams'])
+    return nutritional_information_df
+
+
+def get_gelato_composition_information(gelato_composition: dict):
+    '''
+    Function to calculate the % of every nutrition type (fat, sugar, etc) in the gelato. 
+    
+    There is some confusion regarding how this calculation should be done - whether only those
+    ingredients should be considered that go into the gelato base, or should all the 
+    ingredients be considered that are a part of the final product. Currently, all the 
+    ingredients are considered; so fat from dark chocolate in stracciatela is considered.
+    This calculation is done to ensure that the % target of every nutrition type is in the
+    range suitable for gelato, like how sugar should be within [16, 20].
+    '''
+    for key in gelato_composition.keys():
+        if key == 'Stabilizers (%)':
+            gelato_composition[key] = ingredient_amount['Stabilizer']
+        elif key == 'Emulsifiers (%)':
+            gelato_composition[key] = 0.0
+        elif key == 'Total Fat (%)': 
+            gelato_composition[key] = nutritional_information['Total Fat (g)']
+            continue    # already normalized, so skip percent normalization in this loop
+        elif key == 'Total Sugar (%)':
+            gelato_composition[key] = nutritional_information['Total Sugar (g)']
+            continue    # already normalized, so skip percent normalization in this loop
+        else:
+            for ingredient in selected_ingredients.keys():
+                gelato_composition[key] += get_nutrition(ingredient, key, nutrition_df, 
+                                                                ingredient_amount[ingredient], selected_ingredients)
+        gelato_composition[key] = gelato_composition[key] / total_weight * 100
+    return gelato_composition
 
 # region INGREDIENT INFORMATION
 whipping_cream = {
@@ -21,27 +92,21 @@ whipping_cream = {
     'Protein (g)': 3.17,
     'Carbohydrates (g)': 5.95,
     'Total Sugar (g)': 3.06,
-    'Added Sugar (g)': 0.01,
     'Total Fat (g)': 35,
-    'Trans Fat (g)': 0.01,
-    'Saturated Fat (g)': 25.32,
     'Cholestrol (mg)': 34.88,
     'Sodium (mg)': 1725.45,
     'Calcium (mg)': 0.0,
-    'MSNF (%)': 0.0,
-    'Water (%)': 60,
+    'MSNF (%)': 9.12,
+    'Water (%)': 56.88,
     'Cost/g': 0.58
 }
 amul_taaza = {
     'Name': 'Amul Taaza',
-    'Energy (Kcal)': 57.8,
+    'Energy (Kcal)': 62.0,
     'Protein (g)': 3.0,
     'Carbohydrates (g)': 4.7,
     'Total Sugar (g)': 4.7,
-    'Added Sugar (g)': 0.0,
-    'Total Fat (g)': 3.0,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 2.0,
+    'Total Fat (g)': 3.5,
     'Cholestrol (mg)': 9.0,
     'Sodium (mg)': 40.0,
     'Calcium (mg)': 110.0,
@@ -55,10 +120,7 @@ amul_gold = {
     'Protein (g)': 3.2,
     'Carbohydrates (g)': 5.0,
     'Total Sugar (g)': 5.0,
-    'Added Sugar (g)': 0.0,
     'Total Fat (g)': 6.5,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 4.2,
     'Cholestrol (mg)': 17.0,
     'Sodium (mg)': 42.0,
     'Calcium (mg)': 120.0,
@@ -72,10 +134,7 @@ country_delight = {
     'Protein (g)': 3.2,
     'Carbohydrates (g)': 4.4,
     'Total Sugar (g)': 4.0,
-    'Added Sugar (g)': 0.0,
     'Total Fat (g)': 3.5,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 2.9,
     'Cholestrol (mg)': 7.0,
     'Sodium (mg)': 40.0,
     'Calcium (mg)': 150.0,
@@ -89,10 +148,7 @@ country_delight_full_cream = {
     'Protein (g)': 3.2,
     'Carbohydrates (g)': 4.9,
     'Total Sugar (g)': 4.9,
-    'Added Sugar (g)': 0.0,
     'Total Fat (g)': 6.0,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 3.8,
     'Cholestrol (mg)': 13.0,
     'Sodium (mg)': 45.0,
     'Calcium (mg)': 150.0,
@@ -100,16 +156,27 @@ country_delight_full_cream = {
     'Water (%)': 85,
     'Cost/g': 0.12
 }
+skimmed_milk_powder = {
+    'Name': 'NAKPRO Skimmed Milk Powder',
+    'Energy (Kcal)': 367.0,
+    'Protein (g)': 35.0,
+    'Carbohydrates (g)': 54.0,
+    'Total Sugar (g)': 54.0,
+    'Total Fat (g)': 1.2,
+    'Cholestrol (mg)': 9.0,
+    'Sodium (mg)': 40.0,
+    'Calcium (mg)': 110.0,
+    'MSNF (%)': 95.0,
+    'Water (%)': 3.0,
+    'Cost/g': 0.51
+}
 amul_dark_chocolate_sugarfree = {
     'Name': 'Amul Dark Chocolate Sugar Free',
     'Energy (Kcal)': 558.0,
     'Protein (g)': 6.0,
     'Carbohydrates (g)': 56.7,
     'Total Sugar (g)': 0.5,
-    'Added Sugar (g)': 0.0,
     'Total Fat (g)': 33.8,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 20.7,
     'Cholestrol (mg)': 0.0,
     'Sodium (mg)': 38.0,
     'Calcium (mg)': 0.0,
@@ -123,10 +190,7 @@ popular_essentials_refined_sugar = {
     'Protein (g)': 0.0,
     'Carbohydrates (g)': 100.0,
     'Total Sugar (g)': 100.0,
-    'Added Sugar (g)': 100.0,
     'Total Fat (g)': 0.0,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 0.0,
     'Cholestrol (mg)': 0.0,
     'Sodium (mg)': 0.0,
     'Calcium (mg)': 0.0,
@@ -140,10 +204,7 @@ weikfield_cornstarch = {
     'Protein (g)': 0.0,
     'Carbohydrates (g)': 87.2,
     'Total Sugar (g)': 0.0,
-    'Added Sugar (g)': 0.0,
     'Total Fat (g)': 0.0,
-    'Trans Fat (g)': 0.0,
-    'Saturated Fat (g)': 0.0,
     'Cholestrol (mg)': 0.0,
     'Sodium (mg)': 14.7,
     'Calcium (mg)': 0.0,
@@ -156,12 +217,14 @@ weikfield_cornstarch = {
 milk_ingredients = [amul_taaza, amul_gold, country_delight, country_delight_full_cream]
 whipping_cream_ingredients = [whipping_cream]
 refined_sugar_ingredients = [popular_essentials_refined_sugar]
+skimmed_milk_powder_ingredients = [skimmed_milk_powder]
 dark_chocolate_ingredients = [amul_dark_chocolate_sugarfree]
 stabilizer_ingredients = [weikfield_cornstarch]
 emulsifiers_ingredients = []
 all_ingredients = {'Milk': milk_ingredients,
                    'Cream': whipping_cream_ingredients,
                    'Sugar': refined_sugar_ingredients,
+                   'Skimmed Milk Powder': skimmed_milk_powder_ingredients,
                    'Dark Chocolate': dark_chocolate_ingredients,
                    'Stabilizer': stabilizer_ingredients,}
 selected_ingredients = {}
@@ -188,33 +251,29 @@ ingredient_amount = {}
 
 with col_mid:
     st.subheader('Ingredient Amount')
+    st.write('_All amounts in grams, except milk (ml))_')
     for label in all_ingredients.keys():
         if label == 'Milk':
-            ingredient_amount[label] = st.slider(label, 0, 1000, 0, 5)
-        elif label == 'Sugar':
-            ingredient_amount[label] = st.slider(label, 0, 200, 0, 1)
-        elif label == 'Cream' or label == 'Dark Chocolate':
-            ingredient_amount[label] = st.slider(label, 0, 100, 0, 1)
+            ingredient_amount[label] = st.slider(label, 0, 1000, 500, 5)
+        elif label == 'Cream' or label == 'Sugar':
+            ingredient_amount[label] = st.slider(label, 0, 200, 50, 1)
+        elif label == 'Dark Chocolate' or label == 'Skimmed Milk Powder':
+            ingredient_amount[label] = st.slider(label, 0, 100, 50, 1)
         else:
-            ingredient_amount[label] = st.slider(label, 0.0, 10.0, 0.0, 0.1)
+            ingredient_amount[label] = st.slider(label, 0.0, 10.0, 3.0, 0.1)
     
-    for ingredient, ingredient_info in selected_ingredients.items():
-        total_water += (ingredient_info['Water (%)']/100) * ingredient_amount[ingredient]
-        if ingredient == 'Milk':
-            total_weight += (ingredient_amount[ingredient] * milk_density)
-        else:
-            total_weight += ingredient_amount[ingredient]
-        total_cost += ingredient_info['Cost/g'] * ingredient_amount[ingredient]
-
-    st.write('Base weight - ', str(np.round(total_weight, 2)),' grams')
-    st.write('Total cost - INR', str(np.round(total_cost)))
-    st.write('Unit cost - INR', str(np.round(total_cost/total_weight, 2)))
+    total_cost, total_water, total_weight = get_gelato_cost_and_weight(total_cost, total_water, total_weight, 
+                                                                       selected_ingredients, ingredient_amount)
+    
+    st.write(f'**Base weight - {np.round(total_weight, 2)} grams**')
+    st.write(f'**Total cost - INR {np.round(total_cost)}**')
+    st.write(f'**Unit cost - INR {np.round(total_cost/total_weight, 2)}**')
     
 # general gelato composition 
 general_gelato_composition = {
     'Total Fat (%)': [6.0, 9.0],
     'Total Sugar (%)': [16.0, 20.0],
-    'MSNF (%)': [8.0, 11.0],
+    'MSNF (%)': [8.0, 12.0],
     'Stabilizers (%)': [0.25, 0.4],
     'Emulsifiers (%)': [0.0, 0.4],
     'Water (%)': [58.0, 65.0]
@@ -239,37 +298,17 @@ nutritional_information = {
     'Calcium (mg)': 0.0,
 }
 
-for key in nutritional_information.keys():
-    for ingredient in selected_ingredients.keys():
-        nutritional_information[key] += get_total_nutrition(ingredient, key, nutrition_df, 
-                                                            ingredient_amount[ingredient], selected_ingredients)
-    nutritional_information[key] = nutritional_information[key] / total_weight * 100 
-nutritional_information_df = pd.DataFrame.from_dict(nutritional_information, orient = 'index', 
-                                                    columns = ['Value per 100 grams'])
-
-for key in gelato_composition.keys():
-    if key == 'Stabilizers (%)':
-        gelato_composition[key] = ingredient_amount['Stabilizer']
-    elif key == 'Emulsifiers (%)':
-        gelato_composition[key] = 0.0
-    elif key == 'Total Fat (%)': 
-        gelato_composition[key] = nutritional_information['Total Fat (g)']
-        continue    # already normalized, so skip percent normalization in this loop
-    elif key == 'Total Sugar (%)':
-        gelato_composition[key] = nutritional_information['Total Sugar (g)']
-        continue    # already normalized, so skip percent normalization in this loop
-    else:
-        for ingredient in selected_ingredients.keys():
-            gelato_composition[key] += get_total_nutrition(ingredient, key, nutrition_df, 
-                                                            ingredient_amount[ingredient], selected_ingredients)
-    gelato_composition[key] = gelato_composition[key] / total_weight * 100 
+nutritional_information_df = get_gelato_nutritional_information(nutritional_information)
+gelato_composition = get_gelato_composition_information(gelato_composition) 
 
 with col_right:
     st.subheader('Gelato Composition')
+    st.write('_Calculations are done considering all ingredients_')
     for label, range in general_gelato_composition.items():
         st.slider(label, min_value = range[0], max_value = range[1], 
                   value = gelato_composition[label], step = 0.01, disabled = False, 
-                  help = 'min value')
+                  help = f'min value = {range[0]}, max value = {range[1]}')
         
     st.subheader('Nutritional information per 100g')
+    st.write('_Estimate; all ingredients considered_')
     st.write(nutritional_information_df)
